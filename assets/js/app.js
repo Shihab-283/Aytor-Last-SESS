5cbb97ca6c67004b6eec25615c2fb1ecb2e2bc53
Shihab-283 <sadmanshahriar0773@gmail.com>
2023-11-01 20:27:03 +0600

    A-5

--- assets/js/app.js
+++ assets/js/app.js
@@ -89,5 +89,9 @@ $(document).ready(function(){
   const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
   const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+  // Venobox
+
+  new VenoBox();
+
 
 
